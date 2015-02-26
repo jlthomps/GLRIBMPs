b61b9b51745ccd9bf6fd3dace32972f406bff5d1
jlthomps <jlthomps@usgs.gov>
2015-02-26 10:30:11 -0600

    newly created text files and changed AustinMMSD.R to a loop

--- AustinMMSD.R
+++ AustinMMSD.R
@@ -1,92 +1,120 @@
-#Load model coefficients from file
-#set site and parameter to choose correct model coefficients
-modelCoefs <- read.delim(file="C:/Users/jlthomps/Desktop/git/GLRIBMPs/MMSDmodelCoef.csv",stringsAsFactors=FALSE)
-siteNo <- "04087088"
-StartDt <- "2008-11-01"
-EndDt <- "2014-12-31"
-compQW <- 'Cl' # one of c('Cl','Fec','Ec','TSS','TP')
+siteNos <- c("04087119","04087050","04087142","04087030","04087129","04087088")
+startDts <- c("2008-11-01","2008-11-01","2008-11-01","2008-11-01","2008-11-01","2010-02-01")
+endDts <- c("2014-12-31","2014-12-31","2014-12-31","2014-12-31","2014-12-31","2014-12-31")
+loadFiles <- c("AustinDataHoney.RData","AustinDataLittleMenomonee.RData","AustinDataMenomonee16.RData","AustinDataMenomoneeFalls.RData","AustinDataWawa.RData","AustinDataUnderwood.RData")
+compQWs <- c("Cl","Fec","Ec","TSS","TP")
+modelCoefs <- read.delim(file="/Users/jlthomps/GLRIBMPs/MMSDmodelCoef.csv",stringsAsFactors=FALSE)
 
-library(dataRetrieval)
-library(USGSHydroTools)
-dataReg1 <- modelCoefs[which(modelCoefs$type=='R' & modelCoefs$y==compQW & modelCoefs$staid==as.numeric(siteNo)),]
-dataReg2 <- modelCoefs[which(modelCoefs$type=='Q' & modelCoefs$y==compQW & modelCoefs$staid==as.numeric(siteNo)),]
-dataReg1[is.na(dataReg1)] <- 0
-dataReg2[is.na(dataReg2)] <- 0
-
-#load previously saved continuous and merged data for appropriate site
-load("C:/Users/jlthomps/Desktop/git/GLRIBMPs/AustinDataUnderwood.RData")
-
-
-# to calculate daily load
-# user will enter type (daily, monthly, annual), site, start and end date, qw pcode
-# return calculated value, regression type used for calculation, regression eq? plot? other?
-# is data available? check for min and max dates, as well as count. how many can be missing?
-# if available, use regular regression to calculate load, output load and type=regular
-# if not available, or too much missing, check flow. if available, use flow regression to calculate load, output load and type=flow
-# output R-squared?
-
-
-#replace any negative discharge values with 'NA'
-if (min(adaps_disch_in$disch,na.rm=TRUE)<=0) {adaps_disch_in[which(adaps_disch_in$disch<=0),]$disch <- NA}
-#merge continuous data and calculate compQW based on original regression model
-adaps_data_reg <- merge(adaps_cond_in,adaps_turb_in[,c(3,6)],by="pdate",all=TRUE)
-adaps_data_reg <- merge(adaps_data_reg,adaps_temp_in[,c(3,6)],by="pdate",all=TRUE)
-adaps_data_reg$compQWreg1 <- 10 ^ ((log10(adaps_data_reg$cond) * dataReg1$xLogCond) + (log10(adaps_data_reg$turb) * dataReg1$xLogTURB) + (adaps_data_reg$temp * dataReg1$xTemp) + dataReg1$b)
-#merge continuous data frame with discharge data 
-adaps_data_reg <- merge(adaps_data_reg,adaps_disch_in[,c(3,6)],by="pdate",all=TRUE)
-#interpolate discharge data where missing
-library(GSqwsr)
-adaps_data_reg$decYear <- getDecYear(adaps_data_reg$pdate)
-
-mindecYear <- 2010
-maxdecYear <- 2015
-subNum <- (maxdecYear-mindecYear)*4
-adaps_data_reg$dischint <- NA
-for (i in 1:subNum) {
-  startDY <- mindecYear + (i-1)*0.25
-  endDY <- startDY + 0.25
-  adaps_data_reg_sub <- adaps_data_reg[which(adaps_data_reg$decYear>=startDY & adaps_data_reg$decYear<=endDY),]
-  adaps_data_Int <- adaps_data_reg_sub[!is.na(adaps_data_reg_sub$disch),c(1,10)]
-  adaps_data_Int$pdate2 <- as.numeric(adaps_data_Int$pdate)
-  fit <- loess(disch ~ pdate2, adaps_data_Int)
-  adaps_data_reg_sub$dischint <- predict(fit,adaps_data_reg[which(adaps_data_reg$decYear>=startDY & adaps_data_reg$decYear<=endDY),]$pdate)
-  adaps_data_reg[which(adaps_data_reg$decYear>=startDY & adaps_data_reg$decYear<=endDY),]$dischint <- adaps_data_reg_sub$dischint
-  #adaps_data_reg <- merge(adaps_data_reg,adaps_data_reg_sub[,c(1,12)],by="pdate",all=TRUE)
+for (k in 1:6) {
+  siteNo <- siteNos[k]
+  StartDt <- startDts[k]
+  EndDt <- endDts[k]
+  loadFile <- loadFiles[k]
+  #load previously saved continuous and merged data for appropriate site
+  load(paste("/Users/jlthomps/GLRIBMPs/",loadFile,sep=""))
+  for (j in 1:5) {
+    compQW <- compQWs[j] # one of c('Cl','Fec','Ec','TSS','TP')
+    
+    #library(dataRetrieval)
+    #library(USGSHydroTools)
+    dataReg1 <- modelCoefs[which(modelCoefs$type=='R' & modelCoefs$y==compQW & modelCoefs$staid==as.numeric(siteNo)),]
+    dataReg2 <- modelCoefs[which(modelCoefs$type=='Q' & modelCoefs$y==compQW & modelCoefs$staid==as.numeric(siteNo)),]
+    dataReg1[is.na(dataReg1)] <- 0
+    dataReg2[is.na(dataReg2)] <- 0
+    
+    #replace any negative discharge values with 'NA'
+    if (min(adaps_disch_in$disch,na.rm=TRUE)<=0) {adaps_disch_in[which(adaps_disch_in$disch<=0),]$disch <- NA}
+    #merge continuous data and calculate compQW based on original regression model
+    adaps_data_reg <- merge(adaps_cond_in,adaps_turb_in[,c(3,6)],by="pdate",all=TRUE)
+    adaps_data_reg <- merge(adaps_data_reg,adaps_temp_in[,c(3,6)],by="pdate",all=TRUE)
+    adaps_data_reg$compQWreg1 <- 10 ^ ((log10(adaps_data_reg$cond) * dataReg1$xLogCond) + (log10(adaps_data_reg$turb) * dataReg1$xLogTURB) + (adaps_data_reg$temp * dataReg1$xTemp) + dataReg1$b)
+    #merge continuous data frame with discharge data 
+    adaps_data_reg <- merge(adaps_data_reg,adaps_disch_in[,c(3,6)],by="pdate",all=TRUE)
+    #interpolate discharge data where missing
+    library(GSqwsr)
+    adaps_data_reg$decYear <- getDecYear(adaps_data_reg$pdate)
+    
+    mindecYear <- 2010
+    maxdecYear <- 2015
+    subNum <- (maxdecYear-mindecYear)*4
+    adaps_data_reg$dischint <- NA
+    for (i in 1:subNum) {
+      startDY <- mindecYear + (i-1)*0.25
+      endDY <- startDY + 0.25
+      adaps_data_reg_sub <- adaps_data_reg[which(adaps_data_reg$decYear>=startDY & adaps_data_reg$decYear<=endDY),]
+      adaps_data_Int <- adaps_data_reg_sub[!is.na(adaps_data_reg_sub$disch),c(1,10)]
+      adaps_data_Int$pdate2 <- as.numeric(adaps_data_Int$pdate)
+      fit <- loess(disch ~ pdate2, adaps_data_Int)
+      adaps_data_reg_sub$dischint <- predict(fit,adaps_data_reg[which(adaps_data_reg$decYear>=startDY & adaps_data_reg$decYear<=endDY),]$pdate)
+      adaps_data_reg[which(adaps_data_reg$decYear>=startDY & adaps_data_reg$decYear<=endDY),]$dischint <- adaps_data_reg_sub$dischint
+    }
+    if (min(adaps_data_reg$dischint,na.rm=TRUE)<=0) {adaps_data_reg[which(adaps_data_reg$dischint<=0),]$dischint <- NA}
+    adaps_data_reg$dischComb <- ifelse(!is.na(adaps_data_reg$disch),adaps_data_reg$disch,adaps_data_reg$dischint)
+    
+    #calculate compQW based on Q-only regression
+    adaps_data_reg$sinDY <- sin(adaps_data_reg$decYear*2*pi)
+    adaps_data_reg$cosDY <- cos(adaps_data_reg$decYear*2*pi)
+    adaps_data_reg$compQWreg2 <- exp(1) ^ ((log10(adaps_data_reg$dischComb) * dataReg2$xLogQ) + (adaps_data_reg$dischComb * dataReg2$xQ) + (adaps_data_reg$sinDY * dataReg2$sinDY) + (adaps_data_reg$cosDY * dataReg2$cosDY) + dataReg2$b)
+    #create composite compQW based on original model when continuous data is available, then Q-only model
+    adaps_data_reg$compQWreg <- ifelse(!is.na(adaps_data_reg$compQWreg1),adaps_data_reg$compQWreg1,adaps_data_reg$compQWreg2)
+    
+    #calculate instantaneous load flux
+    adaps_data_reg$dischCombL <- adaps_data_reg$dischComb*28.3168466
+    adaps_data_reg$loadFlux <- adaps_data_reg$dischCombL * (adaps_data_reg$compQWreg/1000000)
+    
+    #roll up to daily
+    adaps_data_regLoad <- adaps_data_reg[!is.na(adaps_data_reg$loadFlux),c(1,9,16:19)]
+    temp <- diff(adaps_data_regLoad$pdate,lag=1)
+    temp2 <- diff(adaps_data_regLoad$pdate,lead=1)
+    adaps_data_regLoad$date <- as.Date(adaps_data_regLoad$pdate)
+    temp <- c(0,temp)
+    temp2 <- c(temp2,0)
+    adaps_data_regLoad$diff <- temp
+    adaps_data_regLoad$diff2 <- temp2
+    adaps_data_regLoad$duration <- (temp + temp2)/2
+    adaps_data_regLoad$load <- adaps_data_regLoad$loadFlux * (adaps_data_regLoad$duration*60)
+    adaps_data_regLoad$compQWreg1 <- ifelse(!is.na(adaps_data_regLoad$compQWreg1),1,0)
+    adaps_data_regLoad$compQWreg2 <- ifelse(!is.na(adaps_data_regLoad$compQWreg2) & adaps_data_regLoad$compQWreg1==0,1,0)
+    dailyLoad <- aggregate(load ~ date,data=adaps_data_regLoad,sum)
+    dailyCount <- aggregate(load ~ date,data=adaps_data_regLoad,length)
+    dailyRegCount <- aggregate(compQWreg1 ~ date,data=adaps_data_regLoad,sum)
+    dailyRegQCount <- aggregate(compQWreg2 ~ date,data=adaps_data_regLoad,sum)
+    dailyLoadCounts <- merge(dailyLoad,dailyCount,by="date")
+    dailyLoadCounts <- merge(dailyLoadCounts,dailyRegCount,by="date")
+    dailyLoadCounts <- merge(dailyLoadCounts,dailyRegQCount,by="date")
+    colnames(dailyLoadCounts) <- c("date","loadKg","loadCount","regCount","QregCount")
+    sites <- rep(siteNo,nrow(dailyLoadCounts))
+    compQws <- rep(compQW,nrow(dailyLoadCounts))
+    dailyLoadCounts$site <- sites
+    dailyLoadCounts$compQW <- compQws
+    
+    dailyLoadCounts$month_val <- substr(dailyLoadCounts$date,6,7)
+    dailyLoadCounts$year_val <- substr(dailyLoadCounts$date,1,4)
+    dailyLoadCounts$wy_val <- ifelse(as.numeric(dailyLoadCounts$month_val)>=10,as.character(as.numeric(dailyLoadCounts$year_val)+1),dailyLoadCounts$year_val)
+    annualLoads <- aggregate(loadKg ~ wy_val,data=dailyLoadCounts,sum)
+    
+    fileSave <- paste(siteNo,compQW,"daily.txt",sep="")
+    write.table(dailyLoadCounts,file=fileSave)
+    fileSave <- paste(siteNo,compQW,"annual.txt",sep="")
+    write.table(annualLoads,file=fileSave)
+    
+    dailyLoadCounts$cumLoad <- (cumsum(dailyLoadCounts$loadKg))/1000000
+    mainTxt <- paste("Cumulative load (kilotons) of ",compQW," at station ",siteNo,sep="")
+    pdf(paste(siteNo,compQW,"cumLoadPlot.pdf",sep=""),width=10,height=8)
+    plot(dailyLoadCounts$date,dailyLoadCounts$cumLoad,xlab="Date",type="l",ylab="Cumulative load (kilotons)",main=mainTxt)
+    dev.off()
+    
+    #dailyLoadCounts$pdate <- as.POSIXct(dailyLoadCounts$date,tz="America/Chicago",format="%Y-%m-%d")
+    loadQPlot <- merge(adaps_data_reg,dailyLoadCounts,by.x="pdate",by.y="date",all=TRUE)
+    mainTxt <- paste("Load (",compQW,") and discharge vs time at station ",siteNo,sep="")
+    pdf(paste(siteNo,compQW,"loadQPlot.pdf",sep=""),width=10,height=8)
+    plot(loadQPlot$pdate,loadQPlot$loadKg,xlab="Date",type="p",ylab="Load (kgs)",main=mainTxt,col="red")
+    par(new=T)
+    plot(loadQPlot$pdate,loadQPlot$dischComb,axes=F,xlab="",ylab="",type="l",col="blue")
+    axis(side=4)
+    mtext("Discharge (cfs)",side=4,line=2,col="blue")
+    legend("topright",c("Load (kgs)","Discharge (cfs)"),lty=c(NA,1),lwd=c(2.5,2.5),pch=c(1,NA),col=c("red","blue"))
+    dev.off()
+  }
 }
-if (min(adaps_data_reg$dischint,na.rm=TRUE)<=0) {adaps_data_reg[which(adaps_data_reg$dischint<=0),]$dischint <- NA}
-adaps_data_reg$dischComb <- ifelse(!is.na(adaps_data_reg$disch),adaps_data_reg$disch,adaps_data_reg$dischint)
-
-#calculate compQW based on Q-only regression
-adaps_data_reg$sinDY <- sin(adaps_data_reg$decYear*2*pi)
-adaps_data_reg$cosDY <- cos(adaps_data_reg$decYear*2*pi)
-adaps_data_reg$compQWreg2 <- exp(1) ^ ((log10(adaps_data_reg$dischComb) * dataReg2$xLogQ) + (adaps_data_reg$dischComb * dataReg2$xQ) + (adaps_data_reg$sinDY * dataReg2$sinDY) + (adaps_data_reg$cosDY * dataReg2$cosDY) + dataReg2$b)
-#create composite compQW based on original model when continuous data is available, then Q-only model
-adaps_data_reg$compQWreg <- ifelse(!is.na(adaps_data_reg$compQWreg1),adaps_data_reg$compQWreg1,adaps_data_reg$compQWreg2)
-
-#check to see if any discharge gaps are greater than 1 day
-adaps_data_dischInt <- adaps_data_reg[!is.na(adaps_data_reg$disch),]
-temp <- diff(adaps_data_dischInt$pdate,lag=1)
-adaps_data_dischInt$diff <- c(1,diff(adaps_data_dischInt$pdate,lag=1))
-adaps_data_dischInt_sub <- adaps_data_dischInt[which(adaps_data_dischInt$diff>(60*60*24)),]
-
-#calculate instantaneous load flux
-adaps_data_reg$dischCombL <- adaps_data_reg$dischComb*28.3168466
-adaps_data_reg$loadFlux <- adaps_data_reg$dischCombL * adaps_data_reg$compQWreg
-
-#roll up to daily and check for gaps
-adaps_data_regLoad <- adaps_data_reg[!is.na(adaps_data_reg$loadFlux),c(1,19)]
-temp <- diff(adaps_data_regLoad$pdate,lag=1)
-adaps_data_regLoad$date <- as.Date(adaps_data_regLoad$pdate)
-dailyFlux <- aggregate(loadFlux ~ date,data=adaps_data_regLoad,length)
-
-smalldfQW <- adaps_data_reg[which(adaps_data_reg$pdate<=strptime("2011-10-31","%Y-%m-%d")),]
-smalldfQW <- smalldfQW[!is.na(smalldfQW$compQWreg),]
-smalldfQW <- smalldfQW[!is.na(smalldfQW$dischComb),]
-#smalladaps_disch_in <- adaps_disch_in[which(adaps_disch_in$pdate<=strptime("2012-10-31","%Y-%m-%d")),]
-#smalldfQW <- smalldfQW[which(smalldfQW$pdate %in% smalladaps_disch_in$pdate),]
-n <- nrow(smalldfQW)-1
-smalldfQW <- smalldfQW[c(2:n),]
-instLoad <- LoadInstantaneous(smalldfQW,Conc="compQWreg",sample.time="pdate",Conc2liters=1,df.Q=smalldfQW,Q="dischComb",Q.time="pdate",Q2liters=28.3168466)
-
-
-cl_check <- merge(smalldfQW[,c(1,5,13)],cl_data[,c(15,23)],by.x="pdate",by.y="startDateTime")
+  
--- warrenExampleQ72710.R
+++ warrenExampleQ72710.R
@@ -3,48 +3,55 @@ library(USGSwsStats)
 library(EflowStats)
 library(NWCCompare)
 library(zoo)
-setwd("C:/Users/jlthomps/Desktop/git/GLRIBMPs")
-sites <- read.table("Gaging stations Warren.txt",header=TRUE,sep="\t",stringsAsFactors=FALSE,colClasses="character")
-sites$station <- ifelse(nchar(sites$station)<9,paste('0',sites$station,sep=""),sites$station)
-Q90 <- rep(0,nrow(sites))
-Q72 <- rep(0,nrow(sites))
-Q710 <- rep(0,nrow(sites))
-Q72lm <- rep(0,nrow(sites))
-Q710lm <- rep(0,nrow(sites))
-numYear <- rep(0,nrow(sites))
-minDate <- rep(Sys.Date(),nrow(sites))
-maxDate <- rep(Sys.Date(),nrow(sites))
+setwd("/Users/jlthomps/GLRIBMPs")
+sites <- c("055451345","05435950","05435943","05426500","05408500","05403000","05402500","05400500","05400000","05399000","05398500","05397000","05396000","05394000","05392400","05392000","05370500","040085119","05367000","05364500","05358000","05355500","04084445","04083545","04083500","04083000","04075500","04068000","04028000")
+files <- paste(sites,"data.rdb",sep="")
+#sites <- read.table("Gaging stations Warren.txt",header=TRUE,sep="\t",stringsAsFactors=FALSE,colClasses="character")
+#sites$station <- ifelse(nchar(sites$station)<9,paste('0',sites$station,sep=""),sites$station)
+Q90 <- rep(0,length(sites))
+Q72 <- rep(0,length(sites))
+Q710 <- rep(0,length(sites))
+Q72lm <- rep(0,length(sites))
+Q710lm <- rep(0,length(sites))
+numYear <- rep(0,length(sites))
+minDate <- rep(Sys.Date(),length(sites))
+maxDate <- rep(Sys.Date(),length(sites))
 min7dayperSite <- as.data.frame(cbind(0,0,0))
 colnames(min7dayperSite) <- c("siteNo","year","min7day")
-for (i in 1:nrow(sites)) {
-  site <- sites[i,1]
+for (i in 1:length(sites)) {
+  #site <- sites[i,1]
+  file <- files[i]
   startdate <- "1969-10-01"
   enddate <- "2008-09-30"
   property <- "00060"
-  obs_url <- constructNWISURL(site,property,startdate,enddate,"dv")
-  flowData <- getXMLWML1.1Data(obs_url)
-  if (nrow(flowData)>0) {
-    colnames(flowData) <- c("datetime","flow")
-    Q90[i] <- quantile(flowData$flow,.1,names=FALSE)
-    cat(Q90[i],i,site,sites[i,1],sep=" ")
-    maxDate[i] <- max(flowData$datetime)
-  }
-  else {
-    cat("no data available for site",site,startdate,enddate,sep=" ")
-    Q90[i] <- paste("no data available for site",site,startdate,enddate,sep=" ")
+  dataPath <- "/Users/jlthomps/GLRIBMPs/"
+  flowData <- read.table(paste(dataPath, file, sep = ""), sep = "\t", stringsAsFactors = FALSE, header = TRUE)
+  flowData <- flowData[-1, c(1,3)]
+  flowData$DATE <- strptime(flowData$DATE, format = "%Y%m%d")
+  flowData$VALUE <- as.numeric(flowData$VALUE)
+  flowData1 <- flowData[which(flowData$DATE<=strptime(enddate,format="%Y-%m-%d")),]
+  #obs_url <- constructNWISURL(site,property,startdate,enddate,"dv")
+  #flowData <- getXMLWML1.1Data(obs_url)
+  if (nrow(flowData1)>0) {
+    colnames(flowData1) <- c("datetime","flow")
+    Q90[i] <- quantile(flowData1$flow,.1,names=FALSE,na.rm=TRUE)
+    cat(Q90[i],i,sites[i],sep=" ")
+    maxDate[i] <- max(flowData1$datetime)
+  } else {
+    cat("no data available for site",sites[i],startdate,enddate,sep=" ")
+    Q90[i] <- paste("no data available for site",sites[i],startdate,enddate,sep=" ")
   }
   startdate2 <- "1970-04-01"
   enddate2 <- "2009-03-31"
-  obs_url2 <- constructNWISURL(site,property,startdate2,enddate2,"dv")
-  flowData2 <- getXMLWML1.1Data(obs_url2)
-  if (nrow(flowData2)>0) {
-    flowData2$month_val <- substr(flowData2$date,6,7)
-    flowData2$year_val <- substr(flowData2$date,1,4)
-    flowData2$day_val <- substr(flowData2$date,9,10)
-    flowData2$jul_val <- strptime(flowData2$date,"%Y-%m-%d")$yday+1
+  #obs_url2 <- constructNWISURL(site,property,startdate2,enddate2,"dv")
+  flowData2 <- flowData[which(flowData$DATE>=strptime(startdate2,format="%Y-%m-%d")),]
+  if (nrow(flowData2)>0 & max(flowData2$VALUE,na.rm=TRUE)!=-Inf) {
+    flowData2$month_val <- substr(flowData2$DATE,6,7)
+    flowData2$year_val <- substr(flowData2$DATE,1,4)
+    flowData2$day_val <- substr(flowData2$DATE,9,10)
+    flowData2$jul_val <- strptime(flowData2$DATE,"%Y-%m-%d")$yday+1
     flowData2$wy_val <- ifelse(as.numeric(flowData2$month_val)>=4,as.character(as.numeric(flowData2$year_val)+1),flowData2$year_val)
-    day7mean <- rollmean(flowData2$discharge, 7, align = "right", 
-                         na.pad = TRUE)
+    day7mean <- rollapply(flowData2$VALUE, 7, mean, align = "right", fill = NA)
     day7rollingavg <- data.frame(flowData2, day7mean)
     rollingavgs7day <- subset(day7rollingavg, day7rollingavg$day7mean != 
                                 "NA")
@@ -53,36 +60,49 @@ for (i in 1:nrow(sites)) {
     colnames(min7daybyyear) <- c("year","min7day")
     siteNo <- rep(site,nrow(min7daybyyear))
     min7dayperSite <- rbind(min7dayperSite,cbind(siteNo,min7daybyyear))
+    sort_7day <- sort(min7daybyyear$min7day)
+    rank_50 <- floor(findrank(length(sort_7day), 0.5))
+    if (rank_50 > 0) {
+      l7Q2 <- round(sort_7day[rank_50], digits=2)
+    } else { l7Q2 <- NaN }
+    rank_90 <- floor(findrank(length(sort_7day), 0.9))
+    if (rank_90 > 0) {
+      l7Q10 <- round(sort_7day[rank_90], digits = 2)
+    } else { l7Q10 <- NaN }
     min7daybyyear$rank <- rank(min7daybyyear$min7day)
     min7daybyyear$probability <- min7daybyyear$rank/max(min7daybyyear$rank)
     #plot(log10(min7daybyyear$probability),log10(min7daybyyear$min7day))
     min7daybyyear <- min7daybyyear[which(min7daybyyear$min7day>0),]
-    lmfit <- lm(log10(min7daybyyear$min7day) ~ (log10(min7daybyyear$probability)))
+    if (nrow(min7daybyyear)>0) {
     mean7daybyyear <- mean(min7daybyyear$min7day)
     stdev7daybyyear <- sd(min7daybyyear$min7day)
     skewtemp <- min7daybyyear[,1:2]
     colnames(skewtemp) <- c("year","discharge")
     skew7daybyyear <- skew(skewtemp)
-    #abline(lmfit)
+    lmfit <- lm(log10(min7daybyyear$min7day) ~ (log10(min7daybyyear$probability)))
     Q72lm[i] <- round(10^(unname(lmfit[[1]][1]) + unname(lmfit[[1]][2])*log10(0.5)),digits=2)
     Q710lm[i] <- round(10^(unname(lmfit[[1]][1]) + unname(lmfit[[1]][2])*log10(0.1)),digits=2)
-    Q72[i] <- NWCportalL7Q2(flowData2)
-    Q710[i] <- NWCportalL7Q10(flowData2)
-    cat(Q72[i],i,site,sites[i,1],sep=" ")
-    cat(Q710[i],i,site,sites[i,1],sep=" ")
-    minDate[i] <- min(flowData2$date)
+    } else {
+      cat("no non-NA 7-day minimums for site",sites[i],startdate2,enddate2,sep=" ")
+      Q72lm[i] <- paste("no non-NA 7-day minimums available for site",sites[i],startdate,enddate,sep=" ")
+      Q710lm[i] <- paste("no non-NA 7-day minimums available for site",sites[i],startdate,enddate,sep=" ")
+    }
+    Q72[i] <- l7Q2
+    Q710[i] <- l7Q10
+    cat(Q72[i],i,sites[i],sep=" ")
+    cat(Q710[i],i,sites[i],sep=" ")
+    minDate[i] <- min(flowData2$DATE)
     numYear[i] <- nrow(min7daybyyear)
-  }
-  else {
-    cat("no data available for site",site,startdate,enddate,sep=" ")
-    Q72[i] <- paste("no data available for site",site,startdate,enddate,sep=" ")
-    Q710[i] <- paste("no data available for site",site,startdate,enddate,sep=" ")
-    Q72lm[i] <- paste("no data available for site",site,startdate,enddate,sep=" ")
-    Q710lm[i] <- paste("no data available for site",site,startdate,enddate,sep=" ")
+  } else {
+    cat("no data available for site",sites[i],startdate,enddate,sep=" ")
+    Q72[i] <- paste("no data available for site",sites[i],startdate,enddate,sep=" ")
+    Q710[i] <- paste("no data available for site",sites[i],startdate,enddate,sep=" ")
+    Q72lm[i] <- paste("no data available for site",sites[i],startdate,enddate,sep=" ")
+    Q710lm[i] <- paste("no data available for site",sites[i],startdate,enddate,sep=" ")
   }
 }
 min7dayperSite <- min7dayperSite[which(min7dayperSite$siteNo>0),]
 write.table(min7dayperSite,file="annual7dayminimums.txt",sep="\t",row.names=FALSE)
-stationsQ90Q72Q710 <- data.frame(sites$station,Q90,Q72,Q710,Q72lm,Q710lm,minDate,maxDate,numYear,stringsAsFactors=FALSE)
+stationsQ90Q72Q710 <- data.frame(sites,Q90,Q72,Q710,Q72lm,Q710lm,minDate,maxDate,numYear,stringsAsFactors=FALSE)
 write.table(stationsQ90Q72Q710,file="stationsQ90Q72Q710.txt",sep="\t",row.names=FALSE)
 
